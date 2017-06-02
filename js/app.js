19438f7e545a0f1291e4e8468dbfbc7be95da83c
524855 <annguyen@C02S7148FVH8.home>
2017-06-02 02:41:45 -0400

    adding angular

--- js/app.js
+++ js/app.js
@@ -3,7 +3,15 @@ console.clear();
 const app = angular.module('MyApp', []);
 
 app.controller('Main', ['$http',function($http){
+  this.showFirstSection = true;
+  this.showSecondSection = false;
+  this.showThirdSection  = false;
 
  this.welcomeMessage = "Hello From Angular";
+
+
+ this.showFirstSection = () =>{
+
+ }
 
 }]); // end of user controller
