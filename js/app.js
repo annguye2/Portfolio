712bef2be0f3fe574a35158144fa7d1bc0257762
524855 <annguyen@C02S7148FVH8.home>
2017-06-02 02:53:06 -0400

    make section show/hide

--- js/app.js
+++ js/app.js
@@ -3,15 +3,28 @@ console.clear();
 const app = angular.module('MyApp', []);
 
 app.controller('Main', ['$http',function($http){
-  this.showFirstSection = true;
-  this.showSecondSection = false;
-  this.showThirdSection  = false;
+  this.showFirstSectionBoolean  = true;
+  this.showSecondSectionBoolean = false;
+  this.showThirdSectionBoolean  = false;
+  this.welcomeMessage = "Hello From Angular";
 
- this.welcomeMessage = "Hello From Angular";
 
+  this.openFirstSection = () =>{
+    this.showFirstSectionBoolean  = true;
+    this.showSecondSectionBoolean = !this.showFirstSectionBoolean;
+    this.showThirdSectionBoolean  = !this.showFirstSectionBoolean;
+  }
 
- this.showFirstSection = () =>{
+  this.openSecondSection = () =>{
+    this.showSecondSectionBoolean = true;
+    this.showFirstSectionBoolean  = !this.showSecondSectionBoolean;
+    this.showThirdSectionBoolean  = !this.showSecondSectionBoolean;
+  }
 
- }
+  this.openThirdSection = () =>{
+   this.showThirdSectionBoolean = true;
+   this.showFirstSectionBoolean = !this.showThirdSectionBoolean;
+  this.showSecondSectionBoolean  = !this.showThirdSectionBoolean;
 
+  }
 }]); // end of user controller
