605caeb392988577bd6dda0f0e0f7e2d048fc1f2
524855 <annguyen@C02S7148FVH8.home>
2017-06-03 21:30:53 -0400

    added social icon

--- js/app.js
+++ js/app.js
@@ -6,25 +6,35 @@ app.controller('Main', ['$http',function($http){
   this.showFirstSectionBoolean  = true;
   this.showSecondSectionBoolean = false;
   this.showThirdSectionBoolean  = false;
+  this.showFourthSectionBoolean = false;
   this.welcomeMessage = "Hello From Angular";
 
 
   this.openFirstSection = () =>{
-    this.showFirstSectionBoolean  = true;
-    this.showSecondSectionBoolean = !this.showFirstSectionBoolean;
-    this.showThirdSectionBoolean  = !this.showFirstSectionBoolean;
+    this.showFirstSectionBoolean    = true;
+    this.showSecondSectionBoolean   = !this.showFirstSectionBoolean;
+    this.showThirdSectionBoolean    = !this.showFirstSectionBoolean;
+    // this.showFourthSectionBoolean   = !this.showFirstSectionBoolean;
   }
 
   this.openSecondSection = () =>{
-    this.showSecondSectionBoolean = true;
-    this.showFirstSectionBoolean  = !this.showSecondSectionBoolean;
-    this.showThirdSectionBoolean  = !this.showSecondSectionBoolean;
+    this.showSecondSectionBoolean   = true;
+    this.showFirstSectionBoolean    = !this.showSecondSectionBoolean;
+    this.showThirdSectionBoolean    = !this.showSecondSectionBoolean;
+    // this.showFourthSectionBoolean   = !this.showSecondSectionBoolean;
   }
 
   this.openThirdSection = () =>{
-   this.showThirdSectionBoolean = true;
-   this.showFirstSectionBoolean = !this.showThirdSectionBoolean;
-  this.showSecondSectionBoolean  = !this.showThirdSectionBoolean;
+    this.showThirdSectionBoolean  = true;
+    this.showFirstSectionBoolean  = !this.showThirdSectionBoolean;
+    this.showSecondSectionBoolean = !this.showThirdSectionBoolean;
+    this.showFourthSectionBoolean = !this.showThirdSectionBoolean;
+  }
 
+  this.openFourthSection = () =>{
+   this.showFourthSectionBoolean  = true;
+   this.showFirstSectionBoolean   = !this.showFourthSectionBoolean;
+   this.showSecondSectionBoolean  = !this.showFourthSectionBoolean;
+   this.showThirdSectionBoolean   = !this.showFourthSectionBoolean;
   }
 }]); // end of user controller
